c5df33ecde2011f0d1c3a8e0dcdd710264ebba8e
Esin Shadrach <esinshadrachrafe@gmail.com>
2023-08-15 11:31:29 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,32 +17,10 @@ export default function Home() {
 
 	return (
 		<main className="container mx-auto">
-			<div className="typewriter w-fit mx-auto text-center space-y-2">
-				{!show && (
-					<>
-						<H1
-							className={`animation-1 py-2 base-typewriter w-fit`}
-						>
-							Hi, Joy..
-						</H1>
-						<H1 className="animation-2 py-2 base-typewriter w-fit">
-							I&apos;m sorry for what I did.
-						</H1>
-						<H1 className="animation-3 py-2 base-typewriter w-fit">
-							I know it was stupid
-						</H1>
-					</>
-				)}
-				{show && (
-					<>
+			<div className="typewriter w-fit mx-auto text-center space-y-2">		
 						<H1 className="animation-1 py-2 base-typewriter w-fit">
-							but, I am genuinely sorry : (
+						 Deeznuts
 						</H1>
-						<H1 className="animation-1 py-2 base-typewriter w-fit">
-							Please Reply
-						</H1>
-					</>
-				)}
 			</div>
 		</main>
 	);
